e89bcdca7d53d6054126d19e0110676466bdbc45
Tim Bart <tim@sayhello.com>
2016-09-14 16:10:45 -0700

    Consistent use of Sensor model

--- src/main/java/com/hello/suripu/app/sensors/SensorViewFactory.java
+++ src/main/java/com/hello/suripu/app/sensors/SensorViewFactory.java
@@ -1,6 +1,7 @@
 package com.hello.suripu.app.sensors;
 
 import com.google.common.base.Optional;
+import com.hello.suripu.core.models.Sensor;
 import com.hello.suripu.core.roomstate.CurrentRoomState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,27 +21,27 @@ public class SensorViewFactory {
         switch (sensorName) {
             case TEMPERATURE:
                 final SensorView temperature = new SensorView(
-                        "Temperature", SensorName.TEMPERATURE, SensorUnit.CELSIUS, roomState.temperature().value,
+                        "Temperature", Sensor.TEMPERATURE, SensorUnit.CELSIUS, roomState.temperature().value,
                         "some clever message", roomState.temperature().condition(), scale
                 );
                 return Optional.of(temperature);
             case HUMIDITY:
                 final SensorView humidity = new SensorView(
-                        "Humidity", SensorName.HUMIDITY, SensorUnit.PERCENT, roomState.humidity().value, roomState.humidity().message,
+                        "Humidity", Sensor.HUMIDITY, SensorUnit.PERCENT, roomState.humidity().value, roomState.humidity().message,
                         roomState.humidity().condition(), scale);
                 return Optional.of(humidity);
             case LIGHT:
                 final SensorView light = new SensorView(
-                        "Light", SensorName.LIGHT, SensorUnit.LUX, roomState.light().value, roomState.light().message, roomState.light().condition(), scale);
+                        "Light", Sensor.LIGHT, SensorUnit.LUX, roomState.light().value, roomState.light().message, roomState.light().condition(), scale);
                 return Optional.of(light);
             case SOUND:
                 final SensorView sound = new SensorView(
-                        "Sound", SensorName.SOUND, SensorUnit.DB, roomState.sound().value, roomState.sound().message, roomState.sound().condition(), scale);
+                        "Sound", Sensor.SOUND, SensorUnit.DB, roomState.sound().value, roomState.sound().message, roomState.sound().condition(), scale);
                 return Optional.of(sound);
             case PARTICULATES:
                 if(roomState.particulates() != null) {
                     final SensorView airQuality = new SensorView(
-                            "Air quality", SensorName.PARTICULATES, SensorUnit.MG_CM, roomState.particulates().value,
+                            "Air quality", Sensor.PARTICULATES, SensorUnit.MG_CM, roomState.particulates().value,
                             roomState.particulates().message, roomState.particulates().condition(), scale);
                     return Optional.of(airQuality);
                 }
