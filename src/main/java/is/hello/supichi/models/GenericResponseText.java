1999a69a5c4ed545c0c523e7da7ba153f5d471df
KSG <kingshy.goh@gmail.com>
2016-11-22 16:58:03 -0800

    better response text for sleep-sound commands (#220)

--- src/main/java/is/hello/supichi/models/GenericResponseText.java
+++ src/main/java/is/hello/supichi/models/GenericResponseText.java
@@ -1,5 +1,9 @@
 package is.hello.supichi.models;
 
+import com.google.common.base.Optional;
+import is.hello.supichi.commandhandlers.results.GenericResult;
+import is.hello.supichi.commandhandlers.results.Outcome;
+
 /**
  * Created by ksg on 11/3/16
  */
@@ -8,4 +12,14 @@ public class GenericResponseText {
     public static String TRY_AGAIN_TEXT = "Sorry, your command cannot be processed. Please try again.";
     public static String COMMAND_REJECTED_TEXT =  "Sorry, your command is rejected";
     public static String UNKNOWN_TEXT = "Sorry, there was a problem. Please try again later.";
+    public static String OK_TEXT = "Okay.";
+
+    public static String alternativeResponseText(final Optional<GenericResult> result) {
+        if (!result.isPresent()) {
+            return UNKNOWN_TEXT;
+        }
+
+        final Outcome outcome = result.get().outcome;
+        return (outcome.equals(Outcome.OK)) ? OK_TEXT : UNKNOWN_TEXT;
+    }
 }
