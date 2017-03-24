1fb6f53ba40e7e305071f48e3b13c672fb6e7813
kingshy <kingshy@sayhello.com>
2017-03-24 14:38:09 -0700

    update FileInfoDAO to use FileInfoSenseOneDAO

--- src/main/java/is/hello/supichi/Supichi.java
+++ src/main/java/is/hello/supichi/Supichi.java
@@ -26,6 +26,7 @@ import com.hello.suripu.core.db.CalibrationDynamoDB;
 import com.hello.suripu.core.db.DeviceDAO;
 import com.hello.suripu.core.db.DeviceDataDAODynamoDB;
 import com.hello.suripu.core.db.FileInfoDAO;
+import com.hello.suripu.core.db.FileInfoSenseOneDAO;
 import com.hello.suripu.core.db.FileManifestDAO;
 import com.hello.suripu.core.db.FileManifestDynamoDB;
 import com.hello.suripu.core.db.KeyStore;
@@ -178,7 +179,7 @@ public class Supichi
         final ExpansionDataDAO expansionsDataDAO = commonDB.onDemand(ExpansionDataDAO.class);
         final PersistentExpansionDataStore expansionsDataStore = new PersistentExpansionDataStore(expansionsDataDAO);
 
-        final FileInfoDAO fileInfoDAO = commonDB.onDemand(FileInfoDAO.class);
+        final FileInfoDAO fileInfoDAO = commonDB.onDemand(FileInfoSenseOneDAO.class);
         final DeviceDAO deviceDAO = commonDB.onDemand(DeviceDAO.class);
         final SenseColorDAO senseColorDAO = commonDB.onDemand(SenseColorDAOSQLImpl.class);
         final AccountLocationDAO accountLocationDAO = commonDB.onDemand(AccountLocationDAO.class);
